af6b95b8eb75c0d79af87658421015eeb095e52d
Noah <noahshomette@gmail.com>
2024-06-19 14:34:09 -0400

    Add type alias for SquareTilemap

--- src/square/mod.rs
+++ src/square/mod.rs
@@ -3,7 +3,11 @@
 use map_chunk_layer::SquareChunkLayer;
 use map_data::SquareMapData;
 
-use crate::{map::chunk::Chunk, tilemap_builder::TilemapBuilder, tilemap_manager::TilemapManager};
+use crate::{
+    map::{chunk::Chunk, Tilemap},
+    tilemap_builder::TilemapBuilder,
+    tilemap_manager::TilemapManager,
+};
 
 pub mod map_chunk_layer;
 pub mod map_data;
@@ -18,3 +22,6 @@ pub type SquareChunk<TileData> = Chunk<SquareChunkLayer<TileData>, TileData>;
 /// Type alias for [`TilemapBuilder`] for the built in square map types
 pub type SquareTilemapBuilder<TileData, MapLayers> =
     TilemapBuilder<TileData, MapLayers, SquareChunkLayer<TileData>, SquareMapData>;
+
+/// Type alias for [`Tilemap`] for the built in square map tilemap
+pub type SquareTilemap<MapData> = Tilemap<MapData>;
